af1c51a6bbf45cfb75218600f362e247ab00f072
psy <thdud4548@naver.com>
2020-06-24 09:52:45 +0900

    1

--- finalexam01.py
+++ finalexam01.py
@@ -1,7 +1,7 @@
-b=0
+#b=0
 for i in range(1,902,100):
     for a in range(1,i):
-        c=int(4*((-1)(**int(a+1))/(2*i-1)))
-        b+=int(c)
+        c=int(4*pow(-1,int(a+1)))/(2*i-1)
+        b+=c
     print("i m(i)")
     print(i,b)
